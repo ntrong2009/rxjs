d1cef04cfb0345d9afacd5b4da7c321ff3d70e22
ntrong2009 <ngoctrong437@gmail.com>
2022-08-08 10:56:38 +0700

    1 file modified

--- index.ts
+++ index.ts
@@ -3,27 +3,31 @@ import './style.css';
 import { map, interval, timer } from 'rxjs';
 import { tap, retryWhen, delayWhen } from 'rxjs/operators';
 
-// const example = interval(10).pipe(
-//   map((val) => {
-//     if (val > 5) {
-//       throw val;
-//     }
-//     return val;
-//   }),
-//   retryWhen((errors) => {
-//     return errors.pipe(
-//       tap((val) => {
-//         console.log(`value ${val} was too high`);
-//       }),
-//       delayWhen((val) => timer(val * 1000))
-//     );
-//   })
-// );
+const example = interval(10).pipe(
+  map((val) => {
+    if (val > 5) {
+      throw val;
+    }
+    return val;
+  }),
+  retryWhen((errors) => {
+    console.log(errors);
+    return errors.pipe(
+      tap((val) => {
+        console.log(`value ${val} was too high`);
+      }),
+      delayWhen((val) => {
+        console.log(val * 1000);
+        return timer(val * 1000);
+      })
+    );
+  })
+);
 
-// const subscribe = example.subscribe((val) => {
-//   console.log(val);
-// });
-
-timer(1000).subscribe((val) => {
+const subscribe = example.subscribe((val) => {
   console.log(val);
 });
+
+// timer(6000).subscribe((val) => {
+//   console.log(val);
+// });
